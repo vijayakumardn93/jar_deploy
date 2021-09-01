ba72771f32b2a59834ba19f2079c408b0d5786bd
vijayakumar D N <vijaykumar.dn707@gmail.com>
2021-09-01 14:52:43 +0530

    initial commit

---
+++ SpringBoot_Jen/src/main/java/com/example/demo/SpringBootJenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenApplication.class, args);
+	}
+
+}
